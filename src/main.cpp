d5ee8b52f57363a11ce3720495396538ca3b6945
Jamie Terry <jnterry@ntlworld.com>
2017-10-12 23:08:19 +0100

    Implement ticking sound at certian bmp

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,39 @@
 /// \brief Contains entry point for ReactionTimer
 ////////////////////////////////////////////////////////////////////////////
 
+
 #include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 int main(int argc, const char** argv){
+	sf::SoundBuffer tick_buffer;
+	if(!tick_buffer.loadFromFile("tick.ogg")){
+		printf("Failed to load tick.ogg\n");
+		return 1;
+	}
+	sf::Sound tick;
+	tick.setBuffer(tick_buffer);
+
+	int beats_per_minute = 150;
+	float beat_delta = 60.0 / beats_per_minute;
+
+
 	sf::Clock clock;
 
-	while(clock.getElapsedTime().asSeconds() < 2.5){
-		printf("Launching window in: %6.3fs\n", 2.5f - clock.getElapsedTime().asSeconds());
-	}
-
-	// :TODO: If you're using pure OpenGl just use a sf::Window
-	sf::RenderWindow app(sf::VideoMode(640, 400, 32), "ReactionTimer");
-
+	sf::RenderWindow app(sf::VideoMode(640, 400, 32),
+	                     std::string("Cosy Club Reaction Timer")
+	                     );
 	sf::Event event;
-	int frame_counter = 0;
+	float last_beat = clock.getElapsedTime().asSeconds();
 	while(app.isOpen()){
+		float now = clock.getElapsedTime().asSeconds();
 
 		while(app.pollEvent(event)){
 			switch(event.type){
@@ -33,13 +48,20 @@ int main(int argc, const char** argv){
 			}
 		}
 
+		printf("%f\n", now);
+
 		// :TODO: if you're using a pure OpenGl need to do glClear instead
-		app.clear();
+		app.clear(sf::Color((unsigned char)(255 * pow(2, (last_beat - now)*5.0f)),0,0));
 		// Rendering code here
 		app.display();
 
-		++frame_counter;
-		printf("Done frame: %i\n", frame_counter);
+
+		if(now - last_beat >= beat_delta){
+			printf("Tick\n");
+			tick.play();
+			last_beat = now;
+		}
+
 	} // end of main loop
 	printf("Main loop ended\n");
 	return 0;
